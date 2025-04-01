538faf68c4674ba587cab537c7ff53af59248151
Pares <alfariz2004@gmail.com>
2025-04-01 15:18:47 +0700

    connect database and insert data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use sqlx::{Connection, Error, PgConnection, Pool, Postgres};
+    use sqlx::postgres::PgPoolOptions;
+
+    #[tokio::test]
+    async fn test_execute() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        sqlx::query("insert into category(id, name, description) values ('A', 'Contoh', 'Contoh');")
+        .execute(&pool).await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_pool_connection() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        pool.close().await;
+        Ok(())
+
+    }
+
+    async fn get_pool() -> Result<Pool<Postgres>, Error> {
+        let url = "postgres://postgres:keccak-256@localhost:5432/belajar";
+        PgPoolOptions::new()
+            .max_connections(10)
+            .min_connections(5)
+            .acquire_timeout(Duration::from_secs(5))
+            .idle_timeout(Duration::from_secs(60))
+            .connect(url).await
+    }
+
+    #[tokio::test]
+    async fn test_manual_connection() -> Result<(), Error>{
+        let url = "postgres://postgres:keccak-256@localhost:5432/belajar";
+        let connection: PgConnection = PgConnection::connect(url).await?;
+
+        connection.close().await?;
+        Ok(())
+    }
+}
